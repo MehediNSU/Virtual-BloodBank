6d409aca4c03b602c5676fbc0aae146bbd5f3364
Wasimul Islam <wasimul.islam@northsouth.edu>
2021-01-11 16:02:31 +0600

    added the project in the repository

---
+++ app/src/main/java/com/example/virtualbloodbank/Utils/Endpoints.java
@@ -0,0 +1,6 @@
+package com.example.virtualbloodbank.Utils;
+
+public class Endpoints {
+    private static final String base_url = " https://projectcse327.000webhostapp.com";
+    public static final String register_url = base_url+"register.php";
+}
